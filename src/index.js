2338a1ddde59aaee80081740f060450f2d214183
EXCELINDIA\sumukh.putahnkar <sumukh.sathyanarayanaputhankar1@pearson.com>
2024-02-19 17:04:29 +0530

    v1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//we have define components in 2 techinque
+//1.fuction definition area
+
+function A(props) {
+  let fname = "Suraj";
+  let ab = "Gupta";
+  //every function return something
+  return (
+      <span>
+        {fname} {props.mname} and <B lastname={ab} />
+      </span>
+  );
+}
+
+//2.class component
+/*class B extends React.Component{
+1.properties
+
+2.constructor
+
+3.method
+}*/
+class B extends React.Component {
+  fullname = "Rajkumar";
+
+  //2. Constructor
+  // constructor(){
+  //   super();
+  //   this.address="Mysore";
+  // }
+
+
+  //3.method
+  render() {
+    //every fuction return somthing
+    return (
+      <span>
+        {this.fullname} {this.props.lastname} <C>Advani</C> from {this.address}
+      </span>
+    );
+  }
+}
+
+let C=(props)=>{
+
+  let firstname="Mohan";
+  console.log("Hi",props); 
+  return <span>{firstname} {props.children}</span>  
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+let sn = "Kashyap";
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <span>
+    Welcome to <A mname={sn} />
+  </span>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
